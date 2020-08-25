0fc9768fb8c038046c93991f454d2a2d03dde7e3
Ceico <gceico@users.noreply.github.com>
2020-08-25 16:07:27 +0300

    [skip ci] Upload “static/img/hello-dark.js”

---
+++ static/img/hello-dark.js
@@ -0,0 +1,11 @@
+$(document).ready(
+    $(function () {
+        $(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({
+            default_offset_pct: 0.3,
+        })
+        $(".twentytwenty-container[data-orientation='vertical']").twentytwenty({
+            default_offset_pct: 0.3,
+            orientation: 'vertical',
+        })
+    })
+)
