bfe38edae4756530580106512cd1f2c379a63503
Ceicoschi Gabriel <ceicoschi.gabriel@gmail.com>
2021-06-18 13:23:12 +0300

    proper install gatsby-plugin to remove service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,39 @@
 module.exports = {
-  siteMetadata: {
-    title: "Cyscale Website",
-  },
-  flags: { PRESERVE_WEBPACK_CACHE: true },
-  plugins: [
-    'gatsby-plugin-postcss',
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "UA-145426770-1",
-      },
+    siteMetadata: {
+        title: 'Cyscale Website'
     },
-    "gatsby-plugin-react-helmet",
-    // "gatsby-plugin-sitemap",
-    "gatsby-plugin-remove-serviceworker",
-    "gatsby-transformer-remark",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+    flags: { PRESERVE_WEBPACK_CACHE: true },
+    plugins: [
+        'gatsby-plugin-postcss',
+        'gatsby-plugin-sass',
+        'gatsby-plugin-image',
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: 'UA-145426770-1'
+            }
+        },
+        'gatsby-plugin-react-helmet',
+        // "gatsby-plugin-sitemap",
+        'gatsby-plugin-remove-serviceworker',
+        'gatsby-transformer-remark',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/'
+            },
+            __key: 'images'
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/'
+            },
+            __key: 'pages'
+        }
+    ]
 };
