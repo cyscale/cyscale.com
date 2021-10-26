274f30eddef3219ab621c7cbc64560323362a8ca
Ceicoschi Gabriel <ceicoschi.gabriel@gmail.com>
2021-10-26 15:29:27 +0300

    allow html in md so we can add toc

--- src/components/blog-one/paragraphs.js
+++ src/components/blog-one/paragraphs.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-
+import rehypeRaw from 'rehype-raw';
 const ParaGraphs = ({ data }) => {
     const preview = data?.frontmatter?.featuredimage?.publicURL;
     return (
         <>
             <div>
                 <div className='relative lg:absolute xl:absolute 2xl:absolute  top-50 left-51 mb-30px'>
-                    <span className='text-xs text-grey2  block'>Published <strong>{data?.frontmatter?.date?.split('T')?.[0]}</strong></span>
-                    <span className='text-xs text-grey2 block'>By <strong>{data?.frontmatter?.authors}</strong></span>
-                    <span className='text-xs text-grey2 block'>Category <strong>{data?.frontmatter?.category}</strong></span>
+                    <span className='text-xs text-grey2  block'>
+                        Published <strong>{data?.frontmatter?.date?.split('T')?.[0]}</strong>
+                    </span>
+                    <span className='text-xs text-grey2 block'>
+                        By <strong>{data?.frontmatter?.authors}</strong>
+                    </span>
+                    <span className='text-xs text-grey2 block'>
+                        Category <strong>{data?.frontmatter?.category}</strong>
+                    </span>
                 </div>
                 <div className='max-w-588px m-auto relative blog-reset'>
                     {preview && (
-                        <img className='mb-8' src={data?.frontmatter?.featuredimage?.publicURL} alt={data?.frontmatter?.title} />
+                        <img
+                            className='mb-8'
+                            src={data?.frontmatter?.featuredimage?.publicURL}
+                            alt={data?.frontmatter?.title}
+                        />
                     )}
-                    <ReactMarkdown skipHtml={true}>{data && data?.rawMarkdownBody}</ReactMarkdown>
+                    <ReactMarkdown rehypePlugins={[rehypeRaw]}>{data && data?.rawMarkdownBody}</ReactMarkdown>
                 </div>
             </div>
         </>
