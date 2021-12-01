a0f2f094ba445a9a6dfacb1773a48c43e9912504
zolicrisan <92949576+zolicrisan@users.noreply.github.com>
2021-12-01 22:01:01 +0200

    Update cloud-compliance-and-auditing.js

--- src/pages/use-cases/cloud-compliance-and-auditing.js
+++ src/pages/use-cases/cloud-compliance-and-auditing.js
@@ -10,7 +10,7 @@ export default function CloudCompliance() {
             bannerDescription={`Compliance toolbox for cloud-native and cloud-first organizations\n\nThe Cyscale Power Cloud Platform gives you full visibility across cloud and data repos, from app-level to your overall compliance posture. `}
             bannerBtn1Text='Try Cyscale for free'
             bannerBtn1Link='https://app.cyscale.com/'
-            title="Cloud Security Compliance - Cyscale Power Cloud Platform"
+            title="Cloud Security Compliance. Security & Compliance Checks - Cyscale"
             description="Manage, monitor, and prove cloud compliance with Cyscale. Automate checks for 500+ cloud controls & policies to comply with ISO 27001, PCI, SOC 2 & more."
          
        >
