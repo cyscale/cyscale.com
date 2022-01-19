2fb5220bb021b01bedc640ea93928db00236f261
Andrei Stefanie <andrei.stefanie@cyscale.com>
2022-01-19 14:03:18 +0200

    Removed useEffect from HS tracking

--- src/common/hbspotEvents.js
+++ src/common/hbspotEvents.js
@@ -1,19 +1,14 @@
-import { useEffect } from 'react';
 import { useTrackingCode } from 'react-hubspot-tracking-code-hook';
 
 export default function useHubspotEvents({ location, pageName }) {
     const { setPathPageView, setIdentity, setContentType } = useTrackingCode();
 
-    useEffect(() => {
-        const userEmail = localStorage.getItem('user-email');
-        if (userEmail) {
-            setIdentity(userEmail);
-        }
+    const userEmail = localStorage.getItem('user-email');
+    if (userEmail) {
+        setIdentity(userEmail);
+    }
 
-        if (location.pathname) {
-            const contentType = pageName === 'blog-detail' ? 'blog-post' : 'standard-page';
-            setContentType(contentType);
-            setPathPageView(location.pathname);
-        }
-    }, [setPathPageView, setIdentity, setContentType, pageName, location]);
+    const contentType = pageName === 'blog-detail' ? 'blog-post' : 'standard-page';
+    setContentType(contentType);
+    setPathPageView(location.pathname);
 }
