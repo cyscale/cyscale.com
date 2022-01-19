2eefe166eb8423617df20e887026c9a20b04283b
Andrei Stefanie <andrei.stefanie@cyscale.com>
2022-01-19 14:07:58 +0200

    Fix localStorage reference

--- src/common/hbspotEvents.js
+++ src/common/hbspotEvents.js
@@ -3,9 +3,11 @@ import { useTrackingCode } from 'react-hubspot-tracking-code-hook';
 export default function useHubspotEvents({ location, pageName }) {
     const { setPathPageView, setIdentity, setContentType } = useTrackingCode();
 
-    const userEmail = localStorage.getItem('user-email');
-    if (userEmail) {
-        setIdentity(userEmail);
+    if (localStorage) {
+        const userEmail = localStorage.getItem('user-email');
+        if (userEmail) {
+            setIdentity(userEmail);
+        }
     }
 
     const contentType = pageName === 'blog-detail' ? 'blog-post' : 'standard-page';
