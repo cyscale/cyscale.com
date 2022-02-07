ca145a061b0ed0e3d0ef756379fad6624d58af1f
Andrei Stefanie <andrei.stefanie@cyscale.com>
2022-02-07 10:30:52 +0200

    feat: Add misconfig landing page
    CYS-554

---
+++ src/pages/campaigns/misconfigurations.js
@@ -0,0 +1,253 @@
+import React, { useEffect } from 'react';
+import { Container, Row, Section } from '../../components/atoms/Containers';
+import Seo from '../../components/Seo';
+import { Link } from 'gatsby';
+import logo from '../../assets/images/logo.svg';
+import alibaba from '../../assets/images/alibabacloud-icon.svg';
+import aws from '../../assets/images/svg_aws_white.svg';
+import azure from '../../assets/images/svg_azure.svg';
+import gcp from '../../assets/images/svg_gcp.svg';
+import richPolicyMock from '../../assets/images/remote-work-wft-compliance.png';
+import getStartedICon from '../../assets/images/getStartedIconCPSM.svg';
+import GetStarted from '../../components/Home/getstarted';
+import { Link as ScrollLink } from 'react-scroll';
+import { Helmet } from 'react-helmet';
+import arrow from '../../assets/images/arrow.svg';
+import Consent from '../../components/consent';
+import GlobalContext from '../../context/GlobalContext';
+import MisconfigCoverage from '../../components/cloud-security-posture-management/misconfigCoverage';
+import {
+    AutomateCompliance,
+    CutNoise,
+    DetectPreventMisconfigurations
+} from '../../components/cloud-security-posture-management/images';
+
+export default function Misconfigurations({ location }) {
+    useEffect(() => {
+        setTimeout(() => {
+            if (typeof window !== 'undefined' && window['hbspt']) {
+                window.hbspt.forms.create({
+                    portalId: '5413427',
+                    formId: '8543c2b4-5f8e-4df7-9305-aa991806e01f',
+                    target: '#request-demo'
+                });
+            }
+        }, 600);
+    }, []);
+
+    return (
+        <GlobalContext.Provider value={{ location }}>
+            <Seo
+                title='Detect and Elimitate Cloud Misconfigurations'
+                description='Improve your security posture in the cloud, a centralized view of all your assets and servers in one place. Detect and eliminate critical misconfigurations, policy violations, and mistakes.'
+                pageName='MisconfigurationsCampaign'
+                location={location}
+            />
+            <Consent />
+            <Helmet>
+                <script charset='utf-8' type='text/javascript' src='//js.hsforms.net/forms/shell.js' />
+                <meta name='robots' content='noindex' />
+                <meta name='robots' content='nofollow' />
+            </Helmet>
+            <header id='head' className='bg-lightGrey pt-3 pb-2'>
+                <Container>
+                    <Link to='/' className='inline-flex'>
+                        <img className='block ' src={logo} alt='Cyscale' />
+                    </Link>
+                </Container>
+            </header>
+            <main>
+                <section>
+                    <div className='bg-primary py-8'>
+                        <Container>
+                            <Row className='items-center'>
+                                <div className='col-span-12 lg:col-span-5 text-white'>
+                                    <div className='max-w-lg mx-auto lg:ml-0 lg:mr-auto'>
+                                        <h1 className='text-3xl lg:text-4xl leading-normal mb-10'>
+                                            Detect and Elimitate
+                                            <br /> Cloud Misconfigurations
+                                        </h1>
+                                        <p className='mb-6 text-base lg:text-lg leading-relaxed'>
+                                            Improve your security posture in the cloud, a centralized view of all your
+                                            assets and servers in one place. Detect and eliminate critical
+                                            misconfigurations, policy violations, and mistakes.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className='col-span-12 lg:col-span-7'>
+                                    <div className='bg-white rounded-xl shadow-lg  pt-4 pb-0 px-12 max-w-lg mx-auto lg:mr-0 lg:ml-auto relative'>
+                                        <img
+                                            src={arrow}
+                                            className='absolute left-0 top-0 -mt-1 -ml-8'
+                                            width={70}
+                                            alt='arrow'
+                                        />
+                                        <h2 className='text-2xl leading-normal text-primary mb-1'>
+                                            Request a live demo
+                                        </h2>
+                                        <div id='request-demo' style={{ minHeight: 510 }}></div>
+                                    </div>
+                                </div>
+                            </Row>
+                        </Container>
+                    </div>
+                    <div className='bg-grey1 py-4'>
+                        <Container>
+                            <div className='flex justify-between items-center'>
+                                <div className='px-3'>
+                                    <img src={aws} className='h-12' alt='AWS' />
+                                </div>
+                                <div className='px-3'>
+                                    <img src={azure} className='h-12' alt='Azure' />
+                                </div>
+                                <div className='px-3'>
+                                    <img src={gcp} className='h-12' alt='GCP' />
+                                </div>
+
+                                <div className='px-3'>
+                                    <img src={alibaba} className='h-12' alt='Alibaba' />
+                                </div>
+                            </div>
+                        </Container>
+                    </div>
+                </section>
+                <Container>
+                    <Section>
+                        <div className='max-w-lg mx-auto lg:mx-0 lg:max-w-none'>
+                            <Row style={{ alignItems: 'center' }}>
+                                <div className='col-span-12 lg:col-span-6 order-2 lg:order-1 lg:pr-8'>
+                                    <h2 className='text-primary text-2xl lg:text-4xl'>
+                                        Prevent misconfigurations & mistakes
+                                    </h2>
+                                    <p className='text-base lg:text-lg leading-relaxed mt-4 lg:mt-8'>
+                                        Cyscale automatically detects and reports violations of security controls and
+                                        compliance risks.
+                                    </p>
+                                    <p className='text-base lg:text-lg leading-relaxed mt-4 lg:mt-8'>
+                                        It's much easier to keep your cloud assets in check with ongoing change
+                                        monitoring that bridges cloud providers.
+                                    </p>
+                                    <p className='text-base lg:text-lg leading-relaxed mt-4 lg:mt-8'>
+                                        Single out misconfigured services and see how failed security controls impact
+                                        compliance from a single, unified view. Plus, you get reliable remediation
+                                        guidelines in the same place.
+                                    </p>
+                                </div>
+                                <div className='col-span-12 lg:col-span-6 order-1 lg:order-2 pb-4 lg:pb-0'>
+                                    <DetectPreventMisconfigurations />
+                                </div>
+                            </Row>
+                        </div>
+                    </Section>
+                    <Section>
+                        <div className='max-w-lg mx-auto lg:mx-0 lg:max-w-none'>
+                            <Row style={{ alignItems: 'center' }}>
+                                <div className='col-span-12 lg:col-span-6 pb-4 lg:pb-0'>
+                                    <CutNoise />
+                                </div>
+                                <div className='col-span-12 lg:col-span-6 lg:pl-8'>
+                                    <h2 className='text-primary text-2xl lg:text-4xl'>Cut through the noise</h2>
+                                    <p className='text-base lg:text-lg leading-relaxed mt-4 lg:mt-8'>
+                                        Get relevant alerts when your cloud assets drift away from established security
+                                        and compliance standards.
+                                    </p>
+                                    <p className='text-base lg:text-lg leading-relaxed mt-4 lg:mt-8'>
+                                        Make Cyscale your single source of truth for CSPM and never miss an important
+                                        security event.
+                                    </p>
+                                    <p className='text-base lg:text-lg leading-relaxed mt-4 lg:mt-8'>
+                                        Move beyond bulky, list-based management. Cyscale's Security Knowledge Graph™
+                                        makes precise correlations between all your cloud assets and data repositories
+                                        to automatically pinpoint critical security improvements.
+                                    </p>
+                                </div>
+                            </Row>
+                        </div>
+                    </Section>
+                    <Section>
+                        <div className='max-w-lg mx-auto lg:mx-0 lg:max-w-none'>
+                            <Row style={{ alignItems: 'center' }}>
+                                <div className='col-span-12 lg:col-span-6 order-2 lg:order-1 lg:pr-8'>
+                                    <h2 className='text-primary text-2xl lg:text-4xl'>Automate compliance checks</h2>
+                                    <p className='text-base lg:text-lg leading-relaxed mt-4 lg:mt-8'>
+                                        Use Cyscale for governance automation across cloud providers and internal teams
+                                        to ensure consistent security and compliance.
+                                    </p>
+                                    <p className='text-base lg:text-lg leading-relaxed mt-4 lg:mt-8'>
+                                        We keep your clouds under continuous assessment and provide in-app security
+                                        consultancy so you make the most of your time and effort.
+                                    </p>
+                                    <p className='text-base lg:text-lg leading-relaxed mt-4 lg:mt-8'>
+                                        Coming soon: create custom Controls that tap into our Security Knowledge Graph™
+                                        and automate the security and compliance checks that matter the most for your
+                                        organization.
+                                    </p>
+                                </div>
+                                <div className='col-span-12 lg:col-span-6 order-1 lg:order-2 pb-4 lg:pb-0'>
+                                    <AutomateCompliance />
+                                </div>
+                            </Row>
+                        </div>
+                    </Section>
+                </Container>
+                <MisconfigCoverage />
+                <div className='gradientBGCSPM pt-60px pb-60px'>
+                    <GetStarted
+                        icon={getStartedICon}
+                        btnComponent={
+                            <ScrollLink smooth={true} duration={500} to='head'>
+                                Request Live Demo
+                            </ScrollLink>
+                        }
+                        description='See Cyscale in action'
+                    />
+                </div>
+            </main>
+            <footer className=' bg-gray  bg-footer-texture'>
+                <Container>
+                    <div className='flex-col md:flex-row flex justify-between py-20 max-w-3xl mx-auto'>
+                        <div className='order-10 md:order-1'>
+                            <small className='text-white text-sm'>
+                                &copy; {new Date().getFullYear()} Cyscale Limited
+                            </small>
+                        </div>
+                        <div className='order-1'>
+                            <a
+                                href='/policies/terms-of-use'
+                                className='font-light text-white no-underline hover:underline text-sm'
+                            >
+                                Terms of use
+                            </a>
+                        </div>
+                        <div className='order-1'>
+                            <a
+                                href='/policies/security-policy'
+                                className='font-light text-white no-underline hover:underline text-sm'
+                            >
+                                Security Policy
+                            </a>
+                        </div>
+                        <div className='order-1'>
+                            <a
+                                href='/policies/privacy-policy'
+                                className='font-light text-white no-underline hover:underline text-sm'
+                            >
+                                Privacy Policy{' '}
+                            </a>
+                        </div>
+                        <div className='order-1'>
+                            <a
+                                href='https://status.cyscale.com'
+                                rel='noopener noreferrer'
+                                target='_blank'
+                                className='font-light text-white no-underline hover:underline text-sm'
+                            >
+                                Status
+                            </a>
+                        </div>
+                    </div>
+                </Container>
+            </footer>
+        </GlobalContext.Provider>
+    );
+}
--- src/pages/use-cases/cloud-misconfigurations.js
+++ src/pages/use-cases/cloud-misconfigurations.js
@@ -12,7 +12,8 @@ import {
 } from '../../components/cloud-security-posture-management/images';
 import Fproducts from '../../components/cloud-security-posture-management/featuredProducts';
 import GetStarted from '../../components/Home/getstarted';
-import { Container, Section, Row } from '../../components/atoms/Containers';
+import MisconfigCoverage from '../../components/cloud-security-posture-management/misconfigCoverage';
+
 const dataFeaturedProducts = [
     {
         tabTitle: 'Prevent misconfigurations, mismanagement and mistakes',
@@ -74,55 +75,7 @@ const Index = ({ location }) => {
             description='Improve your security posture in the cloud, a centralized view of all your assets and servers in one place. Detect and eliminate critical misconfigurations, policy violations, and mistakes.'
         >
             <Fproducts sectionTitle='Cyscale has you covered' data={dataFeaturedProducts} />
-            <div className='bg-lightGreyEEE MisconfigurationsPage'>
-                <Container>
-                    <Section>
-                        <h2 className='text-blue text-34px leading-normal lg:max-w-md sectionTitle new-line mb-12'>
-                            Extensive cloud misconfiguration detection across all of your cloud assets
-                        </h2>
-                        <div className='max-w-xl'>
-                            <Row>
-                                <div className='lg:col-span-6 col-span-12'>
-                                    <p>
-                                        <strong>Analyze cloud misconfigurations</strong>
-                                        <br />
-                                        Analyze misconfigurations against the most popular industry and regulatory
-                                        frameworks, including a wide range of CIS control benchmarks, ISO 27001, and SOC
-                                        2.
-                                    </p>
-                                </div>
-                                <div className='lg:col-span-6 col-span-12'>
-                                    <p>
-                                        <strong>Ensure security and compliance</strong>
-                                        <br />
-                                        Prove alignment with the highest industry standards, enforce policies
-                                        consistently across cloud providers and scale compliance efforts without
-                                        overhead.
-                                    </p>
-                                </div>
-                                <div className='lg:col-span-6 col-span-12'>
-                                    <p>
-                                        <strong>400+ unique configuration controls</strong>
-                                        <br />
-                                        Unique controls across 400+ categories, including authentication, data
-                                        protection, logging and monitoring, network configurations, system integrity,
-                                        etc.
-                                    </p>
-                                </div>
-                                <div className='lg:col-span-6 col-span-12'>
-                                    <p>
-                                        <strong> Prioritize misconfiguration risk</strong>
-                                        <br />
-                                        With our Security Knowledge Graph<sup className='no-underline'>TM</sup>, We
-                                        surface crucial issues based on deep understanding of all your interlinked
-                                        assets - rather than a mishmash of unrelated assets.
-                                    </p>
-                                </div>
-                            </Row>
-                        </div>
-                    </Section>
-                </Container>
-            </div>
+            <MisconfigCoverage />
 
             <div className='gradientBG'>
                 <GetStarted
