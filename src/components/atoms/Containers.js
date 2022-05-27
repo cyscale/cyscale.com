195c441190593add2add185b15f87a7f0ffc6809
Gabriel Ceicoschi <ceicoschi.gabriel@gmail.com>
2022-05-27 12:46:34 +0300

    feat: add new page for request demo form

--- src/components/atoms/Containers.js
+++ src/components/atoms/Containers.js
@@ -6,12 +6,12 @@ export const Row = ({ children, className = '', ...rest }) => (
     </div>
 );
 export const Container = ({ children, className, ...rest }) => (
-    <div {...rest} className={'container max-w-7xl m-auto px-4 lg:px-8 ' + className}>
+    <div {...rest} className={'container max-w-7xl m-auto px-4 lg:px-8 ' + className || ''}>
         {children}
     </div>
 );
 export const Section = ({ children, className, ...rest }) => (
-    <section {...rest} className={'pt-24 pb-24 lg:pt-32 lg:pb-32 ' + className}>
+    <section {...rest} className={'pt-24 pb-24 lg:pt-32 lg:pb-32 ' + className || ''}>
         {children}
     </section>
 );
