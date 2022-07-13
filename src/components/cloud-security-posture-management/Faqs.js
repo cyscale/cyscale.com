7f796abfa0d9e714ca513173ede2dddc6679fcd9
Petru <petru.giurgiu@cyscale.com>
2022-07-13 15:08:24 +0300

    fix: fix duplicate faq data on cloud-security-posture-management page

--- src/components/cloud-security-posture-management/Faqs.js
+++ src/components/cloud-security-posture-management/Faqs.js
@@ -1,98 +1,146 @@
 import { Link } from 'gatsby';
 import React, { useState } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import { Row } from '../atoms/Containers';
 import arrow from '../../assets/images/arrow-left.svg';
 import classNames from 'classnames';
+import { Helmet } from 'react-helmet';
 
-const qas = [
-    {
-        question: 'What is a Cloud Security Posture Management (CSPM) tool?',
-        answer: (
-            <>
-                <p className='mb-4'>
-                    CSPM is a group of security tools and technologies that empower security users to identify and
-                    remediate risk and misconfigurations in their cloud environments.
-                </p>
-                <Link className='underline' to='/blog/cloud-security-posture-management-cspm-guide/'>
-                    Read more in our guide
-                </Link>
-                .
-            </>
-        )
-    },
-    {
-        question: 'What are some key capabilities of a CSPM solution?',
-        answer: (
-            <ul className='list-disc ml-3 pl-3'>
-                <li className='mb-4'>
-                    Continuous compliance monitoring of your cloud resources, so you can identify and remediate risk in
-                    areas such as storage, encryption, and permissions
-                </li>
-                <li className='mb-4'>
-                    Technical security controls and visibility over access configuration, to help reduce risk
-                    and ensure that compliance standards are met
-                </li>
-                <li className='mb-4'>
-                    Easily identify risks that could compromise your cloud network
-                </li>
+const qas = (component) => {
+    return [
+        {
+            question: 'What is a Cloud Security Posture Management (CSPM) tool?',
+            answer: (
+                <>
+                    <p className='mb-4'>
+                        CSPM is a group of security tools and technologies that empower security users to identify and
+                        remediate risk and misconfigurations in their cloud environments.
+                    </p>
+                    {component ? (
+                        <Link className='underline' to='/blog/cloud-security-posture-management-cspm-guide/'>
+                            Read more in our guide
+                        </Link>
+                    ) : (
+                        <a href='https://cyscale.com/blog/cloud-security-posture-management-cspm-guide/'>
+                            Read more in our guide
+                        </a>
+                    )}
+                    .
+                </>
+            )
+        },
+        {
+            question: 'What are some key capabilities of a CSPM solution?',
+            answer: (
+                <ul className='list-disc ml-3 pl-3'>
+                    <li className='mb-4'>
+                        Continuous compliance monitoring of your cloud resources, so you can identify and remediate risk
+                        in areas such as storage, encryption, and permissions
+                    </li>
+                    <li className='mb-4'>
+                        Technical security controls and visibility over access configuration, to help reduce risk and
+                        ensure that compliance standards are met
+                    </li>
+                    <li className='mb-4'>Easily identify risks that could compromise your cloud network</li>
 
-                <li>Remediation guidance, to quickly solve misconfigurations leading to compliance violations</li>
-            </ul>
-        )
-    },
-    {
-        question: 'What are some benefits of utilizing a CSPM tool?',
-        answer: (
-            <>
-                <p className='mb-4'>
-                    <strong className='text-base'>
-                        Visibility into your company's cloud infrastructure and security configurations.
-                    </strong>
-                    <br />
-                    Security teams can assess posture across multiple cloud environments and accounts through
-                    a centralized dashboard that can give actionable metrics.
-                </p>
-                <p className='mb-4'>
-                    <strong className='text-base'>Evidence.</strong>
-                    <br />
-                    Violations in your cloud infrastructure are mapped to regulatory standards, security frameworks,
-                    as well as internal policies, resulting in evidence collection that enables audits.
-                </p>
-                <p className='mb-4'>
-                    <strong className='text-base'>Reporting and alerting.</strong>
-                    Insights on your cloud's risk posture, as well as alerts and notifications bring risk to your
-                    attention,
-                    enabling investigation and in-time remediation.
-                </p>
-                <p className='mb-4'>
-                    <strong className='text-base'>Automation.</strong>
-                    One of the most important features of a CSPM tool. Automated remediation, helps security teams
-                    reduce the time and effort required by manual remediation, at the same time enabling evidence
-                    collection.
-                </p>
-                <Link className='underline' to='/blog/cloud-security-posture-management-cspm-guide/'>
-                    Read more in our guide
-                </Link>
-                .
-            </>
-        )
-    }
-];
+                    <li>Remediation guidance, to quickly solve misconfigurations leading to compliance violations</li>
+                </ul>
+            )
+        },
+        {
+            question: 'What are some benefits of utilizing a CSPM tool?',
+            answer: (
+                <>
+                    <p className='mb-4'>
+                        <strong className='text-base'>
+                            Visibility into your company's cloud infrastructure and security configurations.
+                        </strong>
+                        <br />
+                        Security teams can assess posture across multiple cloud environments and accounts through a
+                        centralized dashboard that can give actionable metrics.
+                    </p>
+                    <p className='mb-4'>
+                        <strong className='text-base'>Evidence.</strong>
+                        <br />
+                        Violations in your cloud infrastructure are mapped to regulatory standards, security frameworks,
+                        as well as internal policies, resulting in evidence collection that enables audits.
+                    </p>
+                    <p className='mb-4'>
+                        <strong className='text-base'>Reporting and alerting.</strong>
+                        Insights on your cloud's risk posture, as well as alerts and notifications bring risk to your
+                        attention, enabling investigation and in-time remediation.
+                    </p>
+                    <p className='mb-4'>
+                        <strong className='text-base'>Automation.</strong>
+                        One of the most important features of a CSPM tool. Automated remediation, helps security teams
+                        reduce the time and effort required by manual remediation, at the same time enabling evidence
+                        collection.
+                    </p>
+                    {component ? (
+                        <Link className='underline' to='/blog/cloud-security-posture-management-cspm-guide/'>
+                            Read more in our guide
+                        </Link>
+                    ) : (
+                        <a href='https://cyscale.com/blog/cloud-security-posture-management-cspm-guide/'>
+                            Read more in our guide
+                        </a>
+                    )}
+                    .
+                </>
+            )
+        }
+    ];
+};
 
 export default function Faqs() {
     const [active, setActive] = useState(0);
 
     return (
         <div>
+            <Helmet>
+                <script type='application/ld+json' data-rh='true'>{`{
+                        "@context": "https://schema.org",
+                        "@type": "FAQPage",
+                        "mainEntity": [
+                            {
+                                "@type": "Question",
+                                "acceptedAnswer": {
+                                    "@type": "Answer",
+                                    "text": ${JSON.stringify(ReactDOMServer.renderToStaticMarkup(qas(false)[0].answer))}
+                                },
+                                "name": "${qas(false)[0].question}"
+                            },
+                            {
+                                "@type": "Question",
+                                "acceptedAnswer": {
+                                    "@type": "Answer",
+                                    "text": ${JSON.stringify(ReactDOMServer.renderToStaticMarkup(qas(false)[1].answer))}
+                                },
+                                "name": "${qas(false)[1].question}"
+                            },
+                            {
+                                "@type": "Question",
+                                "acceptedAnswer": {
+                                    "@type": "Answer",
+                                    "text": ${JSON.stringify(ReactDOMServer.renderToStaticMarkup(qas(false)[2].answer))}
+                                },
+                                "name": "${qas(false)[2].question}"
+                            }
+                        ]
+                    }`}</script>
+            </Helmet>
+
             <h2 className='font-semibold  text-3xl leading-normal text-white mb-6'>Frequently asked questions</h2>
             <Row>
-                {qas.map((q, key) => (
+                {qas(true).map((q, key) => (
                     <div
                         className='col-span-12 lg:col-span-8 mb-0 bg-white bg-opacity-40 p-4  rounded-md relative'
                         key={key}
                     >
-                        <h3 className='font-semibold  text-base sm:text-lg text-white mb-0 pl-8 leading-snug cursor-pointer select-none'
-                            onClick={() => setActive((s) => (s === key ? null : key))}>
+                        <h3
+                            className='font-semibold  text-base sm:text-lg text-white mb-0 pl-8 leading-snug cursor-pointer select-none'
+                            onClick={() => setActive((s) => (s === key ? null : key))}
+                        >
                             {q.question}
                         </h3>
                         <p className={classNames('text-white mt-4', { block: active === key, hidden: active !== key })}>
--- src/pages/products/cloud-security-posture-management.js
+++ src/pages/products/cloud-security-posture-management.js
@@ -4,7 +4,6 @@ import heroBG from '../../assets/images/hero-CSPM-bg.svg';
 import { useAppLink } from '../../common/links';
 import Cspm from '../../components/cloud-security-posture-management';
 import Layout from '../../components/layout/layout.js';
-import { Helmet } from 'react-helmet';
 
 const SecurityPosture = ({ location }) => {
     const appLink = useAppLink({ location });
@@ -21,38 +20,6 @@ const SecurityPosture = ({ location }) => {
                 title='Cloud Security Posture Management - CSPM Tool - Cyscale'
                 description='Continuous visibility of multi-cloud environments to identify cloud misconfiguration vulnerabilities. Scan, monitor, and remediate across AWS, Azure, Google Cloud, Oracle Cloud'
             >
-                <Helmet>
-                    <script type='application/ld+json' data-rh='true'>{`{
-                        "@context": "https://schema.org",
-                        "@type": "FAQPage",
-                        "mainEntity": [
-                            {
-                                "@type": "Question",
-                                "acceptedAnswer": {
-                                    "@type": "Answer",
-                                    "text": "CSPM is a group of security tools and technologies that empower security users to identify and remediate risk and misconfigurations in their cloud environments."
-                                },
-                                "name": "What is a Cloud Security Posture Management (CSPM) tool?"
-                            },
-                            {
-                                "@type": "Question",
-                                "acceptedAnswer": {
-                                    "@type": "Answer",
-                                    "text": "\n                    <ul>\n                        <li>Continuous compliance monitoring of your cloud resources, so you can identify and remediate risk in\n                        areas such as storage, encryption, and permissions</li>\n                        <li>\n                        Technical security controls and visibility over access configuration, to help reduce risk and ensure that compliance standards are met</li>\n                        <li>Easily identify risks that could compromise your cloud network</li>\n                        <li>Remediation guidance, to quickly solve misconfigurations leading to compliance violations</li>\n                    </ul>\n                "
-                                },
-                                "name": "What are some key capabilities of a CSPM solution?"
-                            },
-                            {
-                                "@type": "Question",
-                                "acceptedAnswer": {
-                                    "@type": "Answer",
-                                    "text": "\n                    <p>Visibility into your company's cloud infrastructure and security configurations.</p>\n                    <p>Security teams can assess posture across multiple cloud environments and accounts through a centralized dashboard that can give actionable metrics.</p>\n                    <p>Evidence.</p>\n                    <p>Violations in your cloud infrastructure are mapped to regulatory standards, security frameworks,\n                    as well as internal policies, resulting in evidence collection that enables audits.</p>\n                    <p>\n                    <strong>Reporting and alerting.</strong>\n                    Insights on your cloud's risk posture, as well as alerts and notifications bring risk to your attention, enabling investigation and in-time remediation.</p>\n                "
-                                },
-                                "name": "What are some benefits of utilizing a CSPM tool?"
-                            }
-                        ]
-                    }`}</script>
-                </Helmet>
                 <Cspm />
             </Layout>
         </>
