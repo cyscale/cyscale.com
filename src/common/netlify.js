4f1772a4f38df994e1df418947915ea8ea35caad
Petru <petru.giurgiu@cyscale.com>
2022-09-21 14:36:20 +0300

    feat: extend netlify image component

--- src/common/netlify.js
+++ src/common/netlify.js
@@ -18,3 +18,57 @@ const BlogPreview = ({ entry }) => (
 );
 
 CMS.registerPreviewTemplate('blog', BlogPreview);
+
+CMS.registerEditorComponent({
+    label: 'Image',
+    id: 'image',
+    fromBlock: (match) =>
+        match && {
+            image: match[1],
+            alt: match[2],
+            title: match[3],
+            classes: match[4]
+        },
+    toBlock: function ({ image, alt, title, classes }, getAsset, fields) {
+        return `<img src="${image || ''}" alt="${alt || ''}" title="${title || ''}" class="${classes || ''}"/>`;
+    },
+    toPreview: ({ image, alt, title, classes }, getAsset, fields) => {
+        return `<img src="${image}" alt="${alt}" title="${title}" class="${classes}"/>`;
+    },
+    pattern: /^<img src="(.*?)" alt="(.*?)" title="(.*?)" class="(.*?)"\/>$/s,
+    fields: [
+        {
+            label: 'Picture',
+            name: 'image',
+            widget: 'image',
+            media_library: {
+                allow_multiple: true
+            }
+        },
+        {
+            label: 'Alt Text',
+            name: 'alt'
+        },
+        {
+            label: 'CSS Classes',
+            name: 'classes',
+            widget: 'select',
+            multiple: true,
+            default: [' shadows '],
+            options: [
+                ' shadows ',
+                ' rounded-sm ',
+                ' rounded ',
+                ' rounded-md ',
+                ' rounded-lg ',
+                ' rounded-2xl ',
+                ' rounded-3xl ',
+                ' rounded-full '
+            ]
+        },
+        {
+            label: 'Title',
+            name: 'title'
+        }
+    ]
+});
