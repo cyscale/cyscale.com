b6b9de13fa432e0e451e2f32b041f9285d6998f0
Petru <petru.giurgiu@cyscale.com>
2022-10-10 09:29:01 +0300

    refactor: change pxs to rems on Loader component

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import tw, { css } from 'twin.macro';
-import CyscaleLoading from '../../assets/images/logo-new-symbol-main.svg';
+import CyscaleLoadingLogo from '../../assets/images/logo-new-symbol-main.svg';
 
 const loaderWrapper = css`
     z-index: 10;
     width: 100%;
     height: 100%;
-    min-width: 80px;
-    max-height: 240px;
+    min-width: 5rem;
+    max-height: 15rem;
     overflow: hidden;
     position: relative;
 `;
@@ -17,7 +17,7 @@ const logo = css`
     transform: translate(-50%, -50%);
     top: 50%;
     left: 50%;
-    width: 32px;
+    width: 2rem;
 `;
 
 const loader = css`
@@ -25,37 +25,37 @@ const loader = css`
     position: absolute;
     left: 50%;
     top: 50%;
-    width: 80px;
-    height: 80px;
-    margin: -40px 0 0 -40px;
+    width: 5rem;
+    height: 5rem;
+    margin: -2.5rem 0 0 -2.5rem;
     border-radius: 50%;
-    border: 2px solid transparent;
+    border: 0.125rem solid transparent;
     border-top-color: #ff4a56;
     z-index: 11;
     ${tw`animate-spin-loader`}
 
     &::before,
-    &::after {
+  &::after {
         content: '';
         position: absolute;
         border-radius: 50%;
-        border: 2px solid transparent;
+        border: 0.125rem solid transparent;
         ${tw`animate-spin-reverse`}
     }
     &::before {
-        top: 3px;
-        left: 3px;
-        right: 3px;
-        bottom: 3px;
+        top: 0.188rem;
+        left: 0.188rem;
+        right: 0.188rem;
+        bottom: 0.188rem;
         opacity: 0.6;
         border-top-color: #0f26aa;
         ${tw`animate-spin-reverse`}
     }
     &::after {
-        top: 9px;
-        left: 9px;
-        right: 9px;
-        bottom: 9px;
+        top: 0.563rem;
+        left: 0.563rem;
+        right: 0.563rem;
+        bottom: 0.563rem;
         opacity: 0.3;
         border-top-color: #ff4a56;
         ${tw`animate-spin-loader-after`}
@@ -65,8 +65,8 @@ const loader = css`
 const overlay = css`
     top: 50%;
     left: 50%;
-    min-width: 100px;
-    min-height: 100px;
+    min-width: 6.25rem;
+    min-height: 6.25rem;
     position: fixed;
     transform: translate(-50%, -50%);
 `;
@@ -74,8 +74,8 @@ const overlay = css`
 function Loader() {
     return (
         <div css={[overlay, loaderWrapper]}>
-            <div css={[loader]} />
-            <img src={CyscaleLoading} alt='loader' css={[logo]} />
+            <div css={loader} />
+            <img src={CyscaleLoadingLogo} alt='loader' css={logo} />
         </div>
     );
 }
