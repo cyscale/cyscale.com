5a7f13538fd04fd9961d49fcd4e83b3288fa50c0
Petru <petru.giurgiu@cyscale.com>
2022-11-25 17:07:34 +0200

    free-trial: WIP

--- src/pages/free-trial.js
+++ src/pages/free-trial.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { Container, Row, Section } from '../components/atoms/Containers';
 import useHSFormLoaded from '../hooks/useHSFormLoaded';
@@ -22,6 +22,16 @@ const heroHeigt = css`
 
 const FreeTrial = ({ location }) => {
     const { loadingForm } = useHSFormLoaded();
+    const [showSectionForm, setShowSection] = useState(true);
+    const hubspotHandler = (event) => {
+        if (
+            event.data.type === 'hsFormCallback' &&
+            event.data.eventName === 'onFormSubmitted' &&
+            event.data.id === 'abd54fce-98b1-41ca-b21a-c375b100edb4'
+        ) {
+            setShowSection(!showSectionForm);
+        }
+    };
 
     useEffect(() => {
         setTimeout(() => {
@@ -33,6 +43,13 @@ const FreeTrial = ({ location }) => {
                 });
             }
         }, 600);
+
+        window.addEventListener('message', hubspotHandler);
+
+        return () => {
+            window.removeEventListener('message', hubspotHandler);
+        };
+        // eslint-disable-next-line
     }, []);
 
     return (
@@ -48,57 +65,61 @@ const FreeTrial = ({ location }) => {
             <div className='bg-hero-campaigns-iam-security' css={heroHeigt}>
                 <Section>
                     <Container>
-                        <Row>
-                            <div className='col-span-12 lg:col-span-6 '>
-                                <div className='max-w-lg mx-auto lg:mx-0'>
-                                    <h1 className='font-semibold text-5xl text-blue mb-12'>Start Free Trial</h1>
-                                    <p className='text-base leading-relaxed max-w-md'>
-                                        Cyscale is an agentless CSPM solution that helps your organization map, secure
-                                        and monitor cloud assets across multiple providers.
-                                    </p>
-                                    <div className='flex mt-6 items-center'>
-                                        <img src={ManFreeTrial} alt='' />{' '}
-                                        <h4 className='ml-2 text-xl font-montserrat font-semibold'>
-                                            1x1 with Cloud Security expert
-                                        </h4>
-                                    </div>
-                                    <div className='flex mt-2 items-center'>
-                                        <img src={TimerFreeTrial} alt='' />{' '}
-                                        <h4 className='ml-2 text-xl font-montserrat font-semibold'>
-                                            Deploy and onboard in 15 minutes
-                                        </h4>
-                                    </div>
-                                    <div className='flex mt-2 items-center'>
-                                        <img src={CloudFreeTrail} alt='' />{' '}
-                                        <h4 className='ml-2 text-xl font-montserrat font-semibold'>
-                                            Get immediate visibility & reporting
-                                        </h4>
-                                    </div>
+                        {showSectionForm && (
+                            <Row>
+                                <div className='col-span-12 lg:col-span-6 '>
+                                    {
+                                        <div className='max-w-lg mx-auto lg:mx-0'>
+                                            <h1 className='font-semibold text-5xl text-blue mb-12'>Start Free Trial</h1>
+                                            <p className='text-base leading-relaxed max-w-md'>
+                                                Cyscale is an agentless CSPM solution that helps your organization map,
+                                                secure and monitor cloud assets across multiple providers.
+                                            </p>
+                                            <div className='flex mt-6 items-center'>
+                                                <img src={ManFreeTrial} alt='' />{' '}
+                                                <h4 className='ml-2 text-xl font-montserrat font-semibold'>
+                                                    1x1 with Cloud Security expert
+                                                </h4>
+                                            </div>
+                                            <div className='flex mt-2 items-center'>
+                                                <img src={TimerFreeTrial} alt='' />{' '}
+                                                <h4 className='ml-2 text-xl font-montserrat font-semibold'>
+                                                    Deploy and onboard in 15 minutes
+                                                </h4>
+                                            </div>
+                                            <div className='flex mt-2 items-center'>
+                                                <img src={CloudFreeTrail} alt='' />{' '}
+                                                <h4 className='ml-2 text-xl font-montserrat font-semibold'>
+                                                    Get immediate visibility & reporting
+                                                </h4>
+                                            </div>
+                                        </div>
+                                    }
                                 </div>
-                            </div>
-                            <div className='col-span-12 lg:col-span-6'>
-                                <div
-                                    className='bg-white rounded-lg mt-6 lg:mt-0 lg:rounded-xl shadow-lg  pt-6 pb-0 px-4 md:px-12 max-w-lg mx-auto lg:mr-0 lg:ml-auto relative'
-                                    style={{
-                                        backgroundColor: 'rgba(255, 255, 255, 0.6)',
-                                        backdropFilter: 'blur(5px)'
-                                    }}
-                                >
-                                    <h2 className='font-bold text-xl lg:text-2xl leading-normal mb-1 font-montserrat'>
-                                        Start Your Cyscale Free Trial
-                                    </h2>
-                                    <p className='font-montserrat font-medium mb-2'>
-                                        Get started today, no credit card required.
-                                    </p>
-                                    {loadingForm && <LoaderContainer minHeight={355} />}
+                                <div className='col-span-12 lg:col-span-6'>
                                     <div
-                                        style={{ minHeight: 355 }}
-                                        id='free-trial-form'
-                                        className={classnames({ hidden: loadingForm })}
-                                    />
+                                        className='bg-white rounded-lg mt-6 lg:mt-0 lg:rounded-xl shadow-lg  pt-6 pb-0 px-4 md:px-12 max-w-lg mx-auto lg:mr-0 lg:ml-auto relative'
+                                        style={{
+                                            backgroundColor: 'rgba(255, 255, 255, 0.6)',
+                                            backdropFilter: 'blur(5px)'
+                                        }}
+                                    >
+                                        <h2 className='font-bold text-xl lg:text-2xl leading-normal mb-1 font-montserrat'>
+                                            Start Your Cyscale Free Trial
+                                        </h2>
+                                        <p className='font-montserrat font-medium mb-2'>
+                                            Get started today, no credit card required.
+                                        </p>
+                                        {loadingForm && <LoaderContainer minHeight={355} />}
+                                        <div
+                                            style={{ minHeight: 355 }}
+                                            id='free-trial-form'
+                                            className={classnames({ hidden: loadingForm })}
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                        </Row>
+                            </Row>
+                        )}
                     </Container>
                 </Section>
             </div>
