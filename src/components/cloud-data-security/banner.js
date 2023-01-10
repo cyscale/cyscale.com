4aa6026457c62740280b9104115604eb330994a3
Petru <petru.giurgiu@cyscale.com>
2023-01-10 11:11:44 +0200

    fix: standardize fonts on cloud-data-security solution page

--- src/components/cloud-data-security/banner.js
+++ src/components/cloud-data-security/banner.js
@@ -1,24 +1,22 @@
-import React from "react";
+import React from 'react';
 
 const Banner = ({ title, description }) => {
-  return (
-    <div>
-      <div className="max-w-1366px mx-auto pt-72 pl-20px pr-20px md:pl-40px md:pr-40px lg:pl-60px lg:pr-60px xl:pl-80px xl:pr-80px 2xl:pl-80px 2xl:pr-80px">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-8 xl:gap-32">
-          <div>
-            <h1 className=" text-blue text-38px sm:text-30px md:text-60px leading-normal sm:leading-normal font-semibold new-line">
-              {title}
-            </h1>
-          </div>
-          <div>
-            <p className="text-16px text-black leading-normal new-line">
-              {description}
-            </p>
-          </div>
+    return (
+        <div>
+            <div className='max-w-1366px mx-auto pt-72 pl-20px pr-20px md:pl-40px md:pr-40px lg:pl-60px lg:pr-60px xl:pl-80px xl:pr-80px 2xl:pl-80px 2xl:pr-80px'>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-8 xl:gap-32'>
+                    <div>
+                        <h1 className=' text-blue text-38px sm:text-30px md:text-60px leading-normal sm:leading-normal font-semibold new-line font-montserrat'>
+                            {title}
+                        </h1>
+                    </div>
+                    <div>
+                        <p className='text-16px text-black leading-normal new-line'>{description}</p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Banner;
