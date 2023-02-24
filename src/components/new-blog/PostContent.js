772e0d1345835915eb896c0146303e592ff5d569
Petru <petru.giurgiu@cyscale.com>
2023-02-24 14:13:20 +0200

    feat: create sections for the 3 categories of links sugestions on the blog

--- src/components/new-blog/PostContent.js
+++ src/components/new-blog/PostContent.js
@@ -5,19 +5,17 @@ import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import Categories from './Categories';
-import Post from './Post';
-import { map, filter, slice } from 'lodash';
 import Share from '../Share/Share';
 import { Link as ScrollLink, Element } from 'react-scroll';
 import { GatsbyImage } from 'gatsby-plugin-image';
-import cyscaleMan from '../../assets/images/cyscale-man.svg';
 import arrowRight from '../../assets/images/arrow-right-1-white.svg';
 import { Link } from 'gatsby';
 import useSubscribe from '../../hooks/useSubscribe';
 import useScrollTrigger from '../scrollTrigger';
 import { css } from 'twin.macro';
-import ComplianceToolbox from '../../assets/images/compliance-toolbox-blog.png';
-import ArrowRight from '../../assets/images/arrow-right-white.svg';
+import ComplianceLinks from './ComplianceLinks';
+import CSPMLinks from './CSPMLinks';
+import OtherLinks from './OtherLinks';
 
 const ctaTransition = css`
     transition: all 0.25s ease-in-out 0s;
@@ -36,7 +34,15 @@ const hrStyle = css`
     width: 13.5rem;
 `;
 
-export default function PostContent({ data, suggestions, preview = false, pageUri, pageName, dataWhitepaper }) {
+export default function PostContent({
+    data,
+    suggestions,
+    preview = false,
+    pageUri,
+    pageName,
+    dataWhitepaper,
+    dataBlueBird
+}) {
     const { emailInput, alert, onChange, onSubmit, onKeyDown } = useSubscribe(pageUri, pageName);
     const trigger = useScrollTrigger();
 
@@ -330,143 +336,9 @@ export default function PostContent({ data, suggestions, preview = false, pageUr
                     </>
                 )}
             </div>
-            {!preview && (
-                <Container>
-                    <div className='mt-24 grid grid-cols-12  md:gap-10'>
-                        <div className='col-span-12 lg:col-span-4 px-4 sm:px-0 sm:mx-auto lg:mx-0 max-w-xs sm:max-w-lg lg:max-w-lg '>
-                            <p
-                                className='text-base font-bold font-montserrat'
-                                css={css`
-                                    color: #38aff1;
-                                `}
-                            >
-                                TOP ARTICLES
-                            </p>
-                            <h2
-                                className='font-bold font-montserrat'
-                                css={css`
-                                    font-size: 2rem;
-                                `}
-                            >
-                                Compliance
-                            </h2>
-                            <div className='flex flex-col'>
-                                {' '}
-                                <Link
-                                    to='/blog/AWS-SOC-2-Compliance-Checklist-A-Detailed-Guide/'
-                                    className='text-base font-medium font-montserrat inline-block mt-4'
-                                >
-                                    AWS SOC 2 Compliance Checklist:
-                                    <br className='hidden lg:block' /> A Detailed Guide
-                                </Link>{' '}
-                                <Link
-                                    to='/blog/cloud-security-and-compliance/'
-                                    className='text-base font-medium font-montserrat inline-block mt-4'
-                                >
-                                    Cloud Security and Compliance: A <br className='hidden lg:block' /> guide for Your
-                                    Cloud Infrastructure
-                                </Link>{' '}
-                                <Link
-                                    to='/blog/pci-dss-compliance-in-cloud/'
-                                    className='text-base font-medium font-montserrat inline-block mt-4'
-                                >
-                                    PCI-DSS Compliance in the Cloud
-                                </Link>{' '}
-                                <Link
-                                    to='/blog/nist-compliance-in-the-cloud/'
-                                    className='text-base font-medium font-montserrat inline-block mt-4'
-                                >
-                                    NIST Compliance in the Cloud
-                                </Link>{' '}
-                                <Link
-                                    to='/blog/hipaa-compliance-in-cloud/'
-                                    className='text-base font-medium font-montserrat inline-block mt-4'
-                                >
-                                    HIPAA Compliance in the Cloud
-                                </Link>
-                            </div>
-                        </div>
-                        <div className='col-span-12 lg:col-span-8 sm:max-w-lg md:max-w-lg lg:max-w-4xl mx-auto lg:mx-0'>
-                            <div
-                                className='mt-6 lg:mt-0 rounded-xl py-6 relative flex'
-                                style={{
-                                    backgroundImage:
-                                        'linear-gradient(to bottom, #4a85eb, #326fe3, #1b58d8, #073fcc, #0623be)'
-                                }}
-                            >
-                                <div className='pl-8 pr-8 xl:pr-0 lg:max-w-md'>
-                                    <h2
-                                        className='text-white font-bold font-montserrat'
-                                        css={css`
-                                            font-size: 2rem;
-                                        `}
-                                    >
-                                        Our Compliance toolbox
-                                    </h2>
-                                    <p className='text-base text-white'>
-                                        Check out our compliance platform for cloud-native and cloud-first
-                                        organizations:
-                                    </p>
-                                    <Link to='/' className='flex mt-4'>
-                                        <img src={ArrowRight} alt='' width={24} />
-                                        <span className='text-base text-white font-montserrat font-bold ml-3'>
-                                            Cloud Security Compliance
-                                        </span>
-                                    </Link>{' '}
-                                    <Link to='/' className='flex mt-4'>
-                                        <img src={ArrowRight} alt='' width={24} />
-                                        <span className='text-base text-white font-montserrat font-bold ml-3'>
-                                            SOC Cloud Security
-                                        </span>
-                                    </Link>{' '}
-                                    <Link to='/' className='flex mt-4'>
-                                        <img src={ArrowRight} alt='' width={24} />
-                                        <span className='text-base text-white font-montserrat font-bold ml-3'>
-                                            PCI Cloud Compliance
-                                        </span>
-                                    </Link>{' '}
-                                    <Link to='/' className='flex mt-4'>
-                                        <img src={ArrowRight} alt='' width={24} />
-                                        <span className='text-base text-white font-montserrat font-bold ml-3'>
-                                            NIST Cloud Compliance
-                                        </span>
-                                    </Link>{' '}
-                                    <Link to='/' className='flex mt-4'>
-                                        <img src={ArrowRight} alt='' width={24} />
-                                        <span className='text-base text-white font-montserrat font-bold ml-3'>
-                                            ISO 27001 Cloud Compliance
-                                        </span>
-                                    </Link>
-                                </div>
-
-                                <div className='hidden lg:block'>
-                                    <img src={ComplianceToolbox} alt="" width={310} className="absolute bottom-0 right-0"/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Container>
-            )}
-            {!preview && suggestions && (
-                <div className='bg-lightGrey2 mt-12 py-12'>
-                    <Container>
-                        <Row className='gap-4 lg:gap-8'>
-                            {map(
-                                slice(
-                                    filter(suggestions, ({ node }) => node.frontmatter.permalink !== data.permalink),
-                                    0,
-                                    3
-                                ),
-                                (post, key) => (
-                                    <div className='col-span-12 md:col-span-6 lg:col-span-4' key={key}>
-                                        {post?.node && <Post {...post.node.frontmatter} />}
-                                    </div>
-                                )
-                            )}
-                        </Row>
-                    </Container>
-                </div>
-            )}
+            {!preview && <ComplianceLinks dataBlueBird={dataBlueBird} />}
+            {!preview && <CSPMLinks dataBlueBird={dataBlueBird} />}
+            {!preview && <OtherLinks dataBlueBird={dataBlueBird} />}
         </div>
     );
 }
