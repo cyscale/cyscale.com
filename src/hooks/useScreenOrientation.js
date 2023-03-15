4b7ccdfb2aa78f0028cd6515d81d7c480d51335b
Petru <petru.giurgiu@cyscale.com>
2023-03-15 08:47:19 +0200

    fix: fix playing video on safari and iOS devices

--- src/hooks/useScreenOrientation.js
+++ src/hooks/useScreenOrientation.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react';
+import { getOrientation, isIPhone, isSafari } from '../common/utils';
 
 const isBrowser = () => typeof window !== 'undefined';
 
 const useScreenOrientation = () => {
-    const [orientation, setOrientation] = useState((isBrowser() && window.screen.orientation.type) || null);
+    const [orientation, setOrientation] = useState(
+        isSafari || isIPhone() ? getOrientation() : (isBrowser() && window.screen.orientation.type) || null
+    );
 
     useEffect(() => {
-        const handleOrientationChange = () => setOrientation(window.screen.orientation.type);
+        const handleOrientationChange = () =>
+            setOrientation(isSafari || isIPhone() ? getOrientation() : window.screen.orientation.type);
 
         window.addEventListener('orientationchange', handleOrientationChange);
         return () => window.removeEventListener('orientationchange', handleOrientationChange);
