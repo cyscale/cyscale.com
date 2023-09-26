61100495047517cd37ded72b844421af983b8918
Petru <petru.giurgiu@cyscale.com>
2023-09-26 17:42:22 +0300

    refactor: Extract duplicated code into separate component

--- src/pages/events/cloud-security-posture-lessons.js
+++ src/pages/events/cloud-security-posture-lessons.js
@@ -2,43 +2,21 @@ import React, { useEffect } from 'react';
 import Layout from '../../components/layout/CleanLayout';
 import { css } from 'twin.macro';
 import { GatsbyImage } from 'gatsby-plugin-image';
-import { graphql, Link, useStaticQuery } from 'gatsby';
-import useScrollTrigger from '../../components/scrollTrigger';
-import classnames from 'classnames';
+import { graphql, useStaticQuery } from 'gatsby';
 import CalendarIcon from '../../assets/images/calendar-icon-webinar.svg';
 import LocationIcon from '../../assets/images/location-icon-webinar.svg';
 import LoaderContainer from '../../components/Loader/LoaderContainer/LoaderContainer';
 import useHSFormLoaded from '../../hooks/useHSFormLoaded';
 import { Helmet } from 'react-helmet';
 import { Element, Link as ScrollLink } from 'react-scroll';
-
-const ctaTransition = css`
-    transition: all 0.25s ease-in-out 0s;
-`;
-
-const ctaWhitepaperTextColor = css`
-    color: #474747;
-`;
-
-const downloadWhitepaperLinkColor = css`
-    color: #0f26aa;
-`;
-
-const hrStyle = css`
-    color: #e3e3e3;
-    width: 13.5rem;
-`;
+import classnames from 'classnames';
+import FurtherReading from '../../components/landing-pages/FurtherReading';
 
 const CloudSecurityPostureLessons = ({ location }) => {
     const { loadingForm } = useHSFormLoaded();
 
     const data = useStaticQuery(graphql`
         query Webinar {
-            whitepaperCover: file(relativePath: { eq: "whitepaper-cover-blog.png" }) {
-                childImageSharp {
-                    gatsbyImageData(width: 111, height: 162, layout: FIXED)
-                }
-            }
             cover: file(relativePath: { eq: "webinar-cover.png" }) {
                 childImageSharp {
                     gatsbyImageData(width: 1080, layout: CONSTRAINED)
@@ -64,7 +42,6 @@ const CloudSecurityPostureLessons = ({ location }) => {
         }, 600);
     }, []);
 
-    const trigger = useScrollTrigger();
     return (
         <Layout
             location={location}
@@ -225,55 +202,7 @@ const CloudSecurityPostureLessons = ({ location }) => {
                         </div>
                     </div>
                 </div>
-                <div className='flex flex-col self-stretch justify-between'>
-                    <div
-                        className={`hidden xl:block w-72 sticky pl-10 font-montserrat pb-16  ${
-                            trigger ? 'top-0' : 'top-28'
-                        }`}
-                        css={ctaTransition}
-                    >
-                        <p
-                            className={classnames({ 'text-xs font-semibold uppercase': true, 'mt-4': trigger })}
-                            css={ctaWhitepaperTextColor}
-                        >
-                            Further reading
-                        </p>
-                        <p className='text-lg font-bold mt-2'>
-                            Cloud Compliance in
-                            <br /> 2023: An In-Depth Guide
-                        </p>
-                        <GatsbyImage
-                            alt='White paper Cover'
-                            className='rounded-md mt-2 shadow-2xl'
-                            image={data.whitepaperCover.childImageSharp.gatsbyImageData}
-                        />
-
-                        <p className='text-xs mt-4 font-montserrat' css={ctaWhitepaperTextColor}>
-                            The whitepaper talks about ISO 27001, SOC 2, PCI-DSS, GDPR, HIPAA.
-                        </p>
-                        <Link
-                            className='text-xs underline'
-                            css={downloadWhitepaperLinkColor}
-                            to={'/whitepaper/the-complete-guide-to-cloud-compliance/'}
-                        >
-                            Download Whitepaper
-                        </Link>
-                        <hr className='mt-4' css={hrStyle} />
-                        <p className='text-lg font-bold mt-4'>
-                            Cloud Storage <br /> Misconfigurations
-                        </p>
-                        <p className='text-xs mt-4 font-montserrat' css={ctaWhitepaperTextColor}>
-                            Build and maintain a strong <br /> Security Program from the start.
-                        </p>
-                        <Link
-                            className='text-xs underline'
-                            css={downloadWhitepaperLinkColor}
-                            to={'/whitepaper/cloud-storage-misconfigurations/'}
-                        >
-                            Download Whitepaper
-                        </Link>
-                    </div>
-                </div>
+                <FurtherReading />
             </div>
         </Layout>
     );
