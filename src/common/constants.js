149eb05794706e47cce0e6169b02fa3861a3bcf2
Petru <petru.giurgiu@cyscale.com>
2024-01-31 17:08:15 +0200

    chore: update search index

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,4 +2,4 @@ export const cookieConsentKey = 'CookiesConsent';
 
 export const algoliaAppID = `041PWC8D0P`;
 export const algoiaSearchKey = `9fd5fa61b586d7219446a40c0650cb46`;
-export const algoliaIndex = `cyscale_website`;
+export const algoliaIndex = `website_cyscale`;
